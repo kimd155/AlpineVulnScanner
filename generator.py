4e95ffe0fa2ea1627fee73879d3ef4f3651ac89e
Kim D <94112981+kimd155@users.noreply.github.com>
2025-03-30 08:46:41 +0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -5,8 +5,9 @@ def generate_report(package_name, package_version, vulnerabilities):
     now = datetime.now()
     timestamp = now.strftime('%H:%M %d/%m/%Y')
 
-    template = Template(open("template.html").read())
+    with open("template.html", encoding='utf-8') as f:
+    	template = Template(f.read())
     report = template.render(package_name=package_name, package_version=package_version, vulnerabilities=vulnerabilities, timestamp=timestamp)
 
-    with open(f'{package_name}_vulnerability_report.html', 'w') as f:
+    with open(f'{package_name}_vulnerability_report.html', 'w', encoding='utf-8') as f:
         f.write(report)
